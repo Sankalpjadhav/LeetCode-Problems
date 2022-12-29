6665f47d05c946f0cfaad1b9d0bf78f6cdb0cf84
Sankalp jadhav <sanky43jadhav@gmail.com>
2022-12-29 09:21:57 +0530

    Time: 187 ms (73.50%), Space: 78 MB (85.94%) - LeetHub

---
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] getOrder(int[][] tasks) {
+        HashMap<int [], Integer> map = new HashMap<>();
+        int n = tasks.length;
+
+        for(int i=0;i<n;i++){
+            map.put(tasks[i], i);
+        }
+
+        Arrays.sort(tasks, (a, b)->a[0]-b[0]);
+
+        PriorityQueue<int []> minHeap = new PriorityQueue<>((a, b) -> {
+            if(a[1] == b[1]){
+                return map.get(a) - map.get(b);
+            }
+            else{
+                return a[1] - b[1];
+            }
+        });
+        int [] result = new int[n];
+        int i = 0;
+        int time = tasks[0][0]; // starting time
+        int j = 0;
+        while(!minHeap.isEmpty() || i<n){
+            while(i<n && time>=tasks[i][0]){
+                minHeap.add(tasks[i]);
+                i++;
+            }
+            
+            if(minHeap.isEmpty()){
+                time = tasks[i][0];
+            }
+            else{
+                int [] task = minHeap.poll();
+                time += task[1];
+                result[j++] = map.get(task);
+            }
+
+        }
+
+
+        return result;
+    }
+}
