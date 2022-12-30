dcfdb16c160463bb7180d7165deec8c776421346
Sankalp jadhav <sanky43jadhav@gmail.com>
2022-12-30 08:13:02 +0530

    Time: 1 ms (100.00%), Space: 44.4 MB (76.49%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -0,0 +1,56 @@
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> result = new ArrayList<>();
+        int n = graph.length;
+        int [] visited = new int[n];
+        List<Integer> path = new ArrayList<>();
+        path.add(0);
+        dfs(0, n-1, graph, result, path);
+        return result;
+    }
+
+    private void dfs(int node, int destination, int [][] graph, List<List<Integer>> result, List<Integer> path){
+        if(node == destination){
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+        for(int adjNode: graph[node]){
+            path.add(adjNode);
+            dfs(adjNode, destination, graph, result, path);
+            path.remove(path.size()-1);
+        }
+    }
+    
+}
+
+/*
+
+    Tried previously:
+    
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        path.add(0);
+        boolean [] visited = new boolean[graph.length];
+        allPaths(graph, 0, result, path, visited);
+        return result;
+    }
+    
+    public void allPaths(int[][] graph, int node, List<List<Integer>> result, List<Integer> path, boolean [] visited){
+        if(node==graph.length-1){
+            result.add(new ArrayList<Integer>(path));
+            return;
+        }
+        visited[node] = true;
+        for(int nextNode: graph[node]){
+            if(visited[nextNode]==false){
+                path.add(nextNode);
+                allPaths(graph, nextNode, result, path, visited);
+                path.remove(path.size()-1);
+            }
+        }
+        visited[node]= false;
+    }
+
+*/
