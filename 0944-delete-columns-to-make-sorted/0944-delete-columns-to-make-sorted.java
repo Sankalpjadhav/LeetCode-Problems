2181d7160990234f4447e20b8304569f0ab116ab
Sankalp jadhav <sanky43jadhav@gmail.com>
2023-01-03 08:25:25 +0530

    Time: 13 ms (55.51%), Space: 48.1 MB (22.70%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
@@ -3,10 +3,14 @@ class Solution {
         int numberOfColsToBeDeleted = 0;
         
         for(int i=0;i<strs[0].length();i++){
-            char ch = '@';
+            char ch = '@'; // ASCII code of @ is smaller than ASCII code of lowercase english letters 
             boolean flag = false;
             for(String str : strs){
-                if(ch > str.charAt(i)){
+                if(ch > str.charAt(i)){ 
+                /*
+                Compare the ASCII value, if previous character ASCII value is greater than current ASCII value 
+                that means they are not sorted lexicographically
+                */
                     numberOfColsToBeDeleted++;
                     flag = true;
                 }
