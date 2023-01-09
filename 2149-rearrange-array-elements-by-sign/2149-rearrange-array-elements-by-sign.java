c58276d856dcac94b97420b0d8aa2073252121d9
Sankalp jadhav <sanky43jadhav@gmail.com>
2023-01-09 08:51:20 +0530

    Time: 4 ms (99.02%), Space: 80 MB (72.69%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,6 +1,28 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         int n = nums.length;
+        int[] res = new int[n];
+        int positive=0, negative=1;
+
+        for(int i=0; i<n; i++){
+            if(nums[i]>0){
+                res[positive]=nums[i];
+                positive += 2;
+            }
+            else{
+                res[negative]=nums[i];
+                negative += 2;
+            }
+        }
+
+        return res;
+    }
+}
+
+/*
+NOT SO EFFICIENT
+    public int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
         
         List<Integer> positiveIntegers = new ArrayList<>();
         List<Integer> negativeIntegers = new ArrayList<>();
@@ -30,4 +52,5 @@ class Solution {
         
         return nums;
     }
-}
+*/
+
