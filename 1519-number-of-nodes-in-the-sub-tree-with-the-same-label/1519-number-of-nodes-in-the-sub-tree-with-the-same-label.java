80ed4fa42605b8977e6fa8d7c4c922394b34c289
Sankalp jadhav <sanky43jadhav@gmail.com>
2023-01-12 09:11:37 +0530

    Time: 89 ms (89.13%), Space: 125 MB (83.70%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.java
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.java
@@ -20,7 +20,9 @@ class Solution {
     
     private int[] dfs(int node, int parent, List<List<Integer>> list, String labels, int [] ans){
         int [] count = new int[26];
+        
         char nodeChar = labels.charAt(node);
+        
         for(int adjNode: list.get(node)){
             if(adjNode == parent) continue;
             int [] childCountArray = dfs(adjNode, node, list, labels, ans);
@@ -34,3 +36,28 @@ class Solution {
         return count;
     }
 }
+
+/*
+Initially got wrong answer for this test case: 
+4
+[[0,2],[0,3],[1,2]]
+"aeed"
+
+Tree:
+
+1     0
+ \  /   \
+   2     3
+
+At the start I made tree using below code as if it is directed edge.
+for(int [] edge : edges){
+    list.get(edge[0]).add(edge[1]);
+}
+
+But based on above tree I understood it will never visit node 1 since we are making dfs call from node 0. 
+So I changed it undirected edge which will cover node 1 from node 2. 
+for(int [] edge : edges){
+    list.get(edge[0]).add(edge[1]);
+    list.get(edge[1]).add(edge[0]);
+}
+*/
