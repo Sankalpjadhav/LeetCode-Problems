9bef65ad75f7c982f0799bcebe5b7d9b76a60d66
Sankalp jadhav <sanky43jadhav@gmail.com>
2023-01-21 22:39:06 +0530

    Time: 211 ms (20.00%), Space: 109.7 MB (20.00%) - LeetHub

---
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.java
@@ -0,0 +1,37 @@
+class Solution {
+    // In contest tried solving using recursion + Memo but it gave TLE bcoz of constraints 1 <= n <= 10^5
+    public long maxScore(int[] nums1, int[] nums2, int k) {
+        List<int[]> nums = new ArrayList<>();
+        int n = nums1.length;
+        for(int i=0; i<n; i++){
+            nums.add(new int[]{nums1[i], nums2[i]});
+        }
+        
+        Collections.sort(nums, (a,b)->b[1]-a[1]);
+        //System.out.println(nums);
+        
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        
+        long total = 0;
+        long best = 0;
+        for(int i=0; i<n; i++){
+            int [] pair = nums.get(i);
+            int x = pair[0];
+            int y = pair[1];
+            
+            minHeap.add(x);
+            total += x;
+            
+            if(minHeap.size() > k){
+                int temp = minHeap.poll();
+                total -= temp;
+            }
+            
+            if(minHeap.size() == k){
+                best = Math.max(best, total*y);
+            }
+        }
+        
+        return best;
+    }
+}
