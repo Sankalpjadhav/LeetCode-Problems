9ac82f942cc8309046d3021e8d1f81159389b337
Sankalp jadhav <sanky43jadhav@gmail.com>
2023-01-21 08:37:05 +0530

    Time: 6 ms (54.94%), Space: 42.5 MB (73.22%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -0,0 +1,24 @@
+class Solution {
+    
+    void helper(String s, String possibleStr, int index, int count, List<String> ans) {
+        if (count > 4) return;
+        if (count == 4 && index == s.length()) {
+            // ans.add(possibleStr); result was ["255.255.11.135.","255.255.111.35."] so we need to remove last dot
+            ans.add(possibleStr.substring(0, possibleStr.length()-1));
+            return;
+        }
+        
+        for (int i = 1; i <= 3 && i+index <= s.length(); i++) {
+            String num = s.substring(index, index + i);
+            if (num.charAt(0) == '0' && i != 1) break;
+            else if (Integer.parseInt(num) <= 255) {
+                helper(s, possibleStr + num + ".", index + i, count + 1, ans);
+            }
+        }
+    }
+    public List<String> restoreIpAddresses(String s) {
+        List<String> ans = new ArrayList<>();
+        helper(s, "", 0, 0, ans);
+        return ans;
+    } 
+}
