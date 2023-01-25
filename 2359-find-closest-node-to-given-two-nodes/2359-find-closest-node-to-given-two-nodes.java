8d0d33292240238a9990d03ed35ade27e8c0ed2c
Sankalp jadhav <sanky43jadhav@gmail.com>
2023-01-25 09:55:56 +0530

    Time: 51 ms (44.31%), Space: 75.4 MB (49.10%) - LeetHub

---
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int closestMeetingNode(int[] edges, int node1, int node2) {
+        int n = edges.length;
+        
+        if(edges == null || n == 0) return -1;
+        if(node1 == node2) return node1;
+        
+        List<List<Integer>> list = new ArrayList<>();
+        
+        for(int i=0; i<n; i++){
+            list.add(new ArrayList<>());
+        }
+        
+        for(int i=0; i<n; i++){
+            list.get(i).add(edges[i]);
+        }
+        
+        boolean [] visitedBy1 = new boolean[n];
+        boolean [] visitedBy2 = new boolean[n];
+       
+        int [] dist1 = new int[n];
+        Arrays.fill(dist1, -1); // Initially marking dist of other nodes from node1 as -1 which means they are not visited by any distance
+        
+        dfs(edges, node1, dist1, visitedBy1, 0);
+        
+        int [] dist2 = new int[n];
+        Arrays.fill(dist2, -1); // Initially marking dist of other nodes from node2 as -1 which means they are not visited by any distance
+        
+        dfs(edges, node2, dist2, visitedBy2, 0);
+        
+        int result = -1;
+        int minDist = Integer.MAX_VALUE;
+        for(int i=0; i<n; i++){
+            if(dist1[i]!=-1 && dist2[i]!=-1){
+                if(minDist > Math.max(dist1[i], dist2[i])){
+                    result = i;
+                    minDist = Math.max(dist1[i], dist2[i]);
+                }
+            }
+        }
+        
+        return result;
+    }
+    
+    
+    private void dfs(int nums[], int node, int dist[], boolean visited[], int currDist){
+        if(node!=-1 && !visited[node]){
+            visited[node]=true;
+            dist[node]=currDist;
+            dfs(nums,nums[node],dist,visited,currDist+1);
+        }
+    }
+}
