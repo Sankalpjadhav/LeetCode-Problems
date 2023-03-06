bcf5399f153342e6a3df167f67c813702cdbb8e7
Sankalp jadhav <sanky43jadhav@gmail.com>
2023-03-06 08:14:00 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (38.09%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,5 +1,25 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
+        int left = 0, right = arr.length;
+        
+        while(left < right){
+            int mid = left + (right - left)/2;
+            if(arr[mid] - (mid + 1) >= k){
+                right = mid;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        return left + k;
+    }
+}
+
+/*
+Time complexity: n + k
+Space complexity: n
+
+public int findKthPositive(int[] arr, int k) {
         int n = k + arr.length;
         Set<Integer> set = new HashSet<>();
         
@@ -18,4 +38,4 @@ class Solution {
         
         return 0;
     }
-}
+*/
