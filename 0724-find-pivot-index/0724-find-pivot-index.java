efcf72ffb7e2e769b5a6fe6c4e94b6b9a14ef2b1
Sankalp jadhav <sanky43jadhav@gmail.com>
2023-07-08 19:13:10 +0530

    Time: 1 ms (100.00%), Space: 44.1 MB (53.00%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -2,6 +2,35 @@ class Solution {
     public int pivotIndex(int[] nums) {
         // Brute Force
         int n = nums.length;
+        if(n == 1){
+            return 0;
+        }
+        
+        int [] prefix = new int[n];
+        int [] suffix = new int[n];
+        
+        for(int i=1;i<n;i++){
+            prefix[i] = prefix[i-1] + nums[i-1];
+        }
+        
+        for(int i=n-2;i>=0;i--){
+            suffix[i] = suffix[i+1] + nums[i+1];
+        }
+        
+        for(int j=0;j<n;j++){
+            if(prefix[j] == suffix[j]){
+                return j;
+            }
+        }
+        
+        return -1;
+    }
+}
+
+/*
+    public int pivotIndex(int[] nums) {
+        // Brute Force
+        int n = nums.length;
         
         for(int i=0;i<n;i++){
             int leftSum = 0;
@@ -20,4 +49,4 @@ class Solution {
         
         return -1;
     }
-}
+*/
