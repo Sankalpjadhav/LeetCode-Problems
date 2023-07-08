1f00f151533bdfe132bb82a5346b9e89feae3c6c
Sankalp jadhav <sanky43jadhav@gmail.com>
2023-07-08 14:20:02 +0530

    Time: 1 ms (100.00%), Space: 45.7 MB (6.50%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        if(n == 1){
+            return;
+        }
+        
+        int index = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i] != 0){
+                nums[index++] = nums[i];
+            }
+        }
+        
+        for(int j=index;j<n;j++){
+            nums[j] = 0;
+        }
+        
+    }
+}
