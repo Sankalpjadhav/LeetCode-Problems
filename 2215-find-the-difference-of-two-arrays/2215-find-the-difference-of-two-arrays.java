df1e6b2e98a9b395c982f60a2245a11463a259cb
Sankalp jadhav <sanky43jadhav@gmail.com>
2023-07-08 19:31:29 +0530

    Time: 15 ms (31.58%), Space: 44.7 MB (53.78%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
@@ -1,34 +1,36 @@
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map1 = new HashMap<>();
+        HashMap<Integer, Integer> map2 = new HashMap<>();
+        
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        
         List<List<Integer>> result = new ArrayList<>();
         
         for(int i=0;i<2;i++){
             result.add(new ArrayList<>());
         }
         
-        Set<Integer> set1 = new HashSet<>();
-        
-        for(int num: nums1){
-            set1.add(num);
+        for(int num:nums1){
+            map1.put(num, 1);
         }
-        
-        Set<Integer> set2 = new HashSet<>();
         
         for(int num: nums2){
-            set2.add(num);
+            map2.put(num, 1);
         }
         
-        for(int num: nums1){
-            if(!set2.contains(num)){
-                set2.add(num); // To handle case Input: nums1 = [1,2,3,3], nums2 = [1,1,2,2], Output: [[3],[]] but my O/P was coming [[3,3],[]]
-                result.get(0).add(num);
+        for(int i=0;i<nums1.length;i++){
+            if(!map2.containsKey(nums1[i]) && !set1.contains(nums1[i])){
+                set1.add(nums1[i]);
+                result.get(0).add(nums1[i]);
             }
         }
         
-        for(int num: nums2){
-            if(!set1.contains(num)){
-                set1.add(num);
-                result.get(1).add(num);
+        for(int i=0;i<nums2.length;i++){
+            if(!map1.containsKey(nums2[i]) && !set2.contains(nums2[i])){
+                set2.add(nums2[i]);
+                result.get(1).add(nums2[i]);
             }
         }
         
