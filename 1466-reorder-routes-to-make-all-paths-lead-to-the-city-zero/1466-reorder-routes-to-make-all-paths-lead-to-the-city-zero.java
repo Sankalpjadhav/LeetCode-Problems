55f122a41d8ab1bd2bba788baf5d2819712d06a0
Sankalp jadhav <sanky43jadhav@gmail.com>
2023-07-09 19:42:39 +0530

    Time: 51 ms (73.01%), Space: 72.4 MB (57.44%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,34 +1,30 @@
 class Solution {
     public int minReorder(int n, int[][] connections) {
-        List<List<int[]>> adjList = new ArrayList<>();
+        List<List<Integer>> adjList = new ArrayList<>();
         
         for(int i=0;i<n;i++){
             adjList.add(new ArrayList<>());
         }
         
-        for(int [] connection: connections){
-            adjList.get(connection[0]).add(new int[]{connection[1], 1});
-            adjList.get(connection[1]).add(new int[]{connection[0], 0});
+        for(int [] path: connections){
+            adjList.get(path[0]).add(path[1]);
+            adjList.get(path[1]).add(-path[0]);
         }
         
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(0);
         boolean [] visited = new boolean[n];
-        visited[0] = true;
-        int cost = 0;
         
-        while(!queue.isEmpty()){
-            int node = queue.poll();
-            
-            for(int [] adjNode:adjList.get(node)){
-                if(!visited[adjNode[0]]){
-                    visited[adjNode[0]] = true;
-                    cost += adjNode[1];
-                    queue.add(adjNode[0]);
-                }
+        return dfs(0, adjList, visited);
+    }
+    
+    private int dfs(int node, List<List<Integer>> adjList, boolean [] visited){
+        visited[node] = true;
+        int numberOfChanges = 0;
+        for(int adjNode: adjList.get(node)){
+            if(!visited[Math.abs(adjNode)]){
+                numberOfChanges += dfs(Math.abs(adjNode), adjList, visited) + ((adjNode > 0) ? 1 : 0);
             }
         }
         
-        return cost;
+        return numberOfChanges;
     }
 }
