e4550727064cad97e87014c5437e78ce50edc415
Sankalp jadhav <sanky43jadhav@gmail.com>
2023-07-10 23:03:25 +0530

    Time: 43 ms (12.95%), Space: 66.1 MB (5.02%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,81 +1,32 @@
 class Solution {
-    // 2 1D space optimised
     public int maxProfit(int[] prices, int fee) {
         int n = prices.length;
-        int [] next = new int[2];
-        int [] curr = new int[2];
-        for(int ind=n-1;ind>=0;ind--){
-            for(int buy=0;buy<=1;buy++){
-                if(buy == 1){
-                    curr[buy] = Math.max(-prices[ind] + next[0], 0 + next[1]);
-                }
-                else{
-                    curr[buy] = Math.max(prices[ind] - fee + next[1], 0 + next[0]);
-                }
-            }
-            next = curr;
+        /*
+            1 Means Buy
+            0 Means Sell
+        */
+        
+        int [][] memo = new int[n][2];
+        for(int i=0;i<n;i++){
+            Arrays.fill(memo[i], -1);
         }
-        return next[1];
+        return calculateProfit(0, 1, prices, fee, memo);
+    }
+    
+    private int calculateProfit(int index, int buy, int [] prices, int fee, int [][] memo){
+        if(index == prices.length){
+            return 0;
+        }
+        
+        if(memo[index][buy] != -1){
+            return memo[index][buy];
+        }
+        
+        if(buy == 1){
+            return memo[index][buy] = Math.max( -prices[index] + calculateProfit(index+1, 0, prices, fee, memo), 0 + calculateProfit(index+1, 1, prices, fee, memo));    
+        }
+        else{
+            return memo[index][buy] = Math.max( prices[index] - fee + calculateProfit(index+1, 1, prices, fee, memo), 0 + calculateProfit(index+1, 0, prices, fee, memo));
+        }
     }
 }
-
-/*
-Recursion:
-    public int maxProfit(int[] prices, int fee) {
-        int n = prices.length;
-        return calcMaxProfit(0, 1, prices, n, fee);
-    }
-    
-    private int calcMaxProfit(int ind, int buy, int [] prices, int n, int fee){
-        if(ind == n) return 0;
-        
-        if(buy == 1){
-            return Math.max(-prices[ind] + calcMaxProfit(ind+1, 0, prices, n, fee), 0 + calcMaxProfit(ind+1, 1, prices, n, fee));
-        }
-        else{
-            return Math.max(prices[ind] - fee + calcMaxProfit(ind+1, 1, prices, n, fee), 0 + calcMaxProfit(ind+1, 0, prices, n, fee));
-        }
-    }
-    
-Memoization:
-    public int maxProfit(int[] prices, int fee) {
-        int n = prices.length;
-        int [][] dp = new int[n][2];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        return calcMaxProfit(0, 1, prices, n, fee, dp);
-    }
-    
-    private int calcMaxProfit(int ind, int buy, int [] prices, int n, int fee, int [][] dp){
-        if(ind == n) return 0;
-        
-        if(dp[ind][buy]!=-1){
-            return dp[ind][buy];
-        }
-        
-        if(buy == 1){
-            return dp[ind][buy] = Math.max(-prices[ind] + calcMaxProfit(ind+1, 0, prices, n, fee, dp), 0 + calcMaxProfit(ind+1, 1, prices, n, fee, dp));
-        }
-        else{
-            return dp[ind][buy] = Math.max(prices[ind] - fee + calcMaxProfit(ind+1, 1, prices, n, fee, dp), 0 + calcMaxProfit(ind+1, 0, prices, n, fee, dp));
-        }
-    }
-    
-Tabulation:
-    public int maxProfit(int[] prices, int fee) {
-        int n = prices.length;
-        int [][] dp = new int[n+1][2];
-        for(int ind=n-1;ind>=0;ind--){
-            for(int buy=0;buy<=1;buy++){
-                if(buy == 1){
-                    dp[ind][buy] = Math.max(-prices[ind] + dp[ind+1][0], 0 + dp[ind+1][1]);
-                }
-                else{
-                    dp[ind][buy] = Math.max(prices[ind] - fee + dp[ind+1][1], 0 + dp[ind+1][0]);
-                }
-            }
-        }
-        return dp[0][1];
-    }
-*/
