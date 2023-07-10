01a868906f1d39432fc74c723fa741a3c3921aa4
Sankalp jadhav <sanky43jadhav@gmail.com>
2023-07-10 08:59:41 +0530

    Time: 106 ms (60.95%), Space: 58.9 MB (93.94%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.java
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.java
@@ -1,22 +1,22 @@
 class Solution {
-    // In contest tried solving using recursion + Memo but it gave TLE bcoz of constraints 1 <= n <= 10^5
-    // https://www.youtube.com/watch?v=5UU6j1cRQkw
     public long maxScore(int[] nums1, int[] nums2, int k) {
-        List<int[]> nums = new ArrayList<>();
         int n = nums1.length;
-        for(int i=0; i<n; i++){
-            nums.add(new int[]{nums1[i], nums2[i]});
+        
+        List<int[]> list = new ArrayList<>();
+        
+        for(int i=0;i<n;i++){
+            list.add(new int[]{nums1[i], nums2[i]});
         }
         
-        Collections.sort(nums, (a,b)->b[1]-a[1]);
-        //System.out.println(nums);
+        Collections.sort(list, (a,b)->b[1]-a[1]);
         
+        long score = 0;
+        long total = 0;
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         
-        long total = 0;
-        long best = 0;
-        for(int i=0; i<n; i++){
-            int [] pair = nums.get(i);
+        
+        for(int i=0;i<n;i++){
+            int [] pair = list.get(i);
             int x = pair[0];
             int y = pair[1];
             
@@ -24,15 +24,14 @@ class Solution {
             total += x;
             
             if(minHeap.size() > k){
-                int temp = minHeap.poll();
-                total -= temp;
+                total -= minHeap.poll();
             }
             
             if(minHeap.size() == k){
-                best = Math.max(best, total*y);
+                score = Math.max(score, total * y);
             }
         }
         
-        return best;
+        return score;
     }
 }
