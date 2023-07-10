90e6870bdd0b44be447a445d9b33219ce1066148
Sankalp jadhav <sanky43jadhav@gmail.com>
2023-07-10 09:33:40 +0530

    Time: 64 ms (55.04%), Space: 58.8 MB (5.73%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.java
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.java
@@ -1,51 +1,32 @@
 class Solution {
-    // cost, index
-  static final Comparator<int[]> comparator =
-      (a, b) -> {
-        int diff = Integer.compare(a[0], b[0]);
-        if (diff == 0) {
-          return Integer.compare(a[1], b[1]);
+    public long totalCost(int[] costs, int k, int candidates) {
+        PriorityQueue<Integer> leftMinHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> rightMinHeap = new PriorityQueue<>();
+        long ans = 0;
+        int i = 0, j = costs.length - 1;
+        for(int l=0;l<k;l++){
+            while(leftMinHeap.size() < candidates && i<=j){
+                leftMinHeap.add(costs[i++]);
+            }
+            
+            while(rightMinHeap.size() < candidates && j >= i){
+                rightMinHeap.add(costs[j--]);
+            }
+            
+            int minCostFromLeft = leftMinHeap.size() > 0 ? leftMinHeap.peek() : Integer.MAX_VALUE;
+            int minCostFromRight = rightMinHeap.size() > 0 ? rightMinHeap.peek() : Integer.MAX_VALUE;
+            
+            if(minCostFromLeft <= minCostFromRight){
+                ans += minCostFromLeft;
+                leftMinHeap.poll();
+            }
+            else{
+                ans += minCostFromRight;
+                rightMinHeap.poll();
+            }
+            
         }
-        return diff;
-      };
-
-  public long totalCost(int[] costs, int k, int candidates) {
-    final int n = costs.length;
-
-    long result = 0;
-    int start = 0;
-    int end = n - 1;
-
-    Queue<int[]> front = new PriorityQueue<>(comparator);
-    Queue<int[]> tail = new PriorityQueue<>(comparator);
-    for (int i = 0; i < k; i++) {
-      while (front.size() < candidates && start <= end) {
-        front.offer(new int[] {costs[start], start});
-        start++;
-      }
-
-      while (tail.size() < candidates && start <= end) {
-        tail.offer(new int[] {costs[end], end});
-        end--;
-      }
-
-      if (front.isEmpty()) {
-        result += tail.poll()[0];
-        continue;
-      }
-
-      if (tail.isEmpty()) {
-        result += front.poll()[0];
-        continue;
-      }
-
-      if (front.peek()[0] > tail.peek()[0]) {
-        result += tail.poll()[0];
-      } else {
-        result += front.poll()[0];
-      }
+        
+        return ans;
     }
-
-    return result;
-  }
 }
