866b3d26218e0e9dc3c60c7ada72df4bec4b2888
Sankalp jadhav <sanky43jadhav@gmail.com>
2025-02-02 22:03:33 +0530

    Time: 48 ms, Memory: 44.1 MB - LeetHub

--- 21477-469-300-longest-increasing-subsequence/21477-469-300-longest-increasing-subsequence.java
+++ 21477-469-300-longest-increasing-subsequence/21477-469-300-longest-increasing-subsequence.java
@@ -1,6 +1,29 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
+        int [] dp = new int[n];
+        int maxi = -1;
+        Arrays.fill(dp, 1);
+
+        for(int index=1;index<n;index++){
+            for(int prevIndex=0;prevIndex<index;prevIndex++){
+                if(nums[prevIndex] < nums[index] && dp[index] < dp[prevIndex]+1){
+                    dp[index] = dp[prevIndex] + 1;
+                    maxi = Math.max(maxi, dp[index]);
+                }
+            }
+        }
+
+        return maxi == -1 ? 1 : maxi;
+            
+    }
+}
+
+/*
+DP:
+
+public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
         int[][] dp = new int[n + 1][n + 1];
 
         for(int index=n-1;index>=0;index--){
@@ -19,26 +42,6 @@ class Solution {
             
     }
 
-    private int findLIS(int index, int prevIndex, int n, int [] nums, int [][] memo){
-        if(index == n){
-            return 0;
-        }
-
-        if(memo[index][prevIndex+1] != -1){
-            return memo[index][prevIndex+1];
-        }
-
-        int notTake = 0 + findLIS(index+1, prevIndex, n, nums, memo);
-        int take = 0;
-        if(prevIndex == -1 || nums[prevIndex] < nums[index]){
-            take = 1 + findLIS(index+1, index, n, nums, memo);
-        }
-
-        return memo[index][prevIndex+1] = Math.max(notTake, take);
-    }
-}
-
-/*
 Memoization: TC: O(n*n), SC: O(n) + O(n*n)
 
 public int lengthOfLIS(int[] nums) {
